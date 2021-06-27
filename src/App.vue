575b597cb63d284bcbf1f81a8cdffdb8a2426daa
drdilyor <drdilyor@outlook.com>
2021-06-27 11:01:28 +0500

    🌊init + tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="bg-gray-400">
+    hello
+  </p>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
