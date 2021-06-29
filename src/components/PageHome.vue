b9c22bed4d878508dc062aa55d75512d5d84f536
drdilyor <drdilyor@outlook.com>
2021-06-29 20:54:49 +0500

    Improve home

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <div v-for="article in $store.state.articles" :key="article._id"
-         class="border rounded p-4 mb-4">
-      <h3 class="text-xl font-medium mb-2 cursor-pointer" @click="$router.push(`/post/${article.id}`)">
-        {{ article.title }}
-      </h3>
-      <div class="flex">
-        <img class="w-12 h-12 rounded-full mr-2" :src="article.user.profile.picture" alt="Profile">
-        <div>
-          <p class="font-medium">{{ article.user.profile.name }}</p>
-          <p class="text-gray-500">{{ $t('posted') }} {{ articleDate(article.createdAt) }}</p>
+         class="flex border rounded mb-2 hover:bg-gray-100">
+      <div class="p-4">
+        <h3 class="text-xl font-medium mb-2 cursor-pointer" @click="$router.push(`/post/${article.id}`)">
+          {{ article.title }}
+        </h3>
+        <div class="flex">
+          <img class="w-12 h-12 rounded-full mr-2" :src="article.user.profile.picture" alt="Profile">
+          <div>
+            <p class="font-medium">{{ article.user.profile.name }}</p>
+            <p class="text-gray-500">{{ $t('posted') }} {{ articleDate(article.createdAt) }}</p>
+          </div>
         </div>
+        <div class="content mt-4" v-html="article.summary" />
       </div>
-      <div class="content mt-4" v-html="article.summary" />
+      <img v-if="article.images.length" :src="`${article.images[0].src}`" alt=""
+           class="w-1/4 object-cover flex-shrink-0">
     </div>
-    <ui-button class="mx-auto mb-4" type="stroked" @click="loadMore">{{ $t('more') }}</ui-button>
+    <ui-button class="mx-auto mb-4" type="stroked" @click="loadMore">{{ $t('loadMore') }}</ui-button>
   </div>
 </template>
 
@@ -45,11 +49,13 @@ export default defineComponent({
         posted: 'Posted',
         today: 'today',
         more: 'More',
+        loadMore: 'Load more',
       },
       uz: {
         posted: 'Joylashitirilgan',
         today: 'bugun',
         more: "Ko'proq",
+        loadMore: "Ko'proq yuklash",
       },
     }
   }
